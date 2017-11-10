3a8ab5e26820276817952cd617e1f030e1296dd3
Kanae-Yoshikawa <test_mac@iclou.com>
2017-11-10 13:24:43 +0900

    edit cpuid

--- cpuid.c
+++ cpuid.c
@@ -1,9 +1,10 @@
 /*  
-    [cpuid命令でCPU情報を取得]
-    インラインアセンブラを使用してcpuid命令を実行し
-    各レジスタに情報が設定されていることを確認する．
-    cpuid命令の詳しい資料についてはintelのデータシートを参照せよ
-*/
+// [cpuid命令でCPU情報を取得]
+// http://linux-biyori.sakura.ne.jp/program/pr_cpuid.php
+// インラインアセンブラを使用してcpuid命令を実行し
+// 各レジスタに情報が設定されていることを確認する．
+// cpuid命令の詳しい資料についてはintelのデータシートを参照せよ
+ */
 
 
 #include <stdio.h>
@@ -17,6 +18,10 @@ typedef struct {
     unsigned int edx;
 }reg_t;
 
+
+/* cpuid()； cpuidを実行するインラインアセンブラのラッパー関数 */
+/* 引数eaxで渡された値をレジスタeaxに設定しcpuid命令を実行.
+   レジスタeax,ebx,ecx,edxに設定された値を引数で渡されたrに代入 */
 void cpuid(unsigned int eax, reg_t *r)
 {
     asm volatile (
@@ -26,6 +31,8 @@ void cpuid(unsigned int eax, reg_t *r)
             );
     return;
 }
+
+
 int main(void)
 {
     reg_t reg;
@@ -33,7 +40,13 @@ int main(void)
     int cpunum = 0, L2 = 0, L3 = 0;
     unsigned int support = 0;
 
+
     //EAX=0x00000000
+    /* ベンダーコード取得．
+       eaxを0x00でcpuidを実行するとeaxに基本命令の使用可能な最大値
+       （cpuidを使用する場合に使える引数eaxの最大値）が設定され、
+       ebx,ecx,edxにベンダーコードの文字列が返される
+     */
     cpuid(0x00, &reg);
     support = reg.eax;
     memcpy(vendorid, &(reg.ebx), 4);
@@ -41,6 +54,9 @@ int main(void)
     memcpy(&vendorid[8], &(reg.ecx), 4);
     printf("vendor_id\t: %s\n", vendorid);
 
+    /* CPU情報取得 */
+    /* eax=0x01としてcpuidを実行し、CPUの
+       ファミリー、モデル、ステッピングやサポートしている命令等を取得 */
     if(support >= 0x01){
         //EAX=0x00000001
         cpuid(0x01, &reg);
@@ -64,9 +80,13 @@ int main(void)
         printf("\n");
     }
 
-    //EX check
+    /*拡張命令の実行 */
+    /* eaxに使用可能な拡張命令の最大値が返される */
     cpuid(0x80000000, &reg);
     support = reg.eax;
+  
+    
+    
     if(support >= 0x80000004){
         //Processor name
         cpuid(0x80000002, &reg);
