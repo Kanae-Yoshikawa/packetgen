c112bce8d7878ad6c50defb3e17d5ebdcedbb6e4
Kanae-Yoshikawa <test_mac@iclou.com>
2017-12-03 14:22:22 +0900

    remove unnecessary variable

--- monica.c
+++ monica.c
@@ -101,8 +101,6 @@ typedef struct _EtherHeader EtherPacket;
 #define ETH_P_Exp   0x0800          // Ethernet type = IP
 //#define ETH_P_Exp   0x86dd        // Ethernet type = IPv6
 //#define ETH_P_Exp   0x0806        // Ethernet type = ARP  address resolution
-//#define ETH_P_Exp   0x88b5        // Ethernet type = IEEE 802.1 Local Experimental Ethertype 1
-                                    // 88b5はinterface cardによって使用不可
 
 #define InitialReplyDelay   40      // これ何???
 #define MaxCommCount        9999    // sendとreceiveのtotal上限回数
@@ -117,7 +115,7 @@ typedef struct _EtherHeader EtherPacket;
 /**************************************************************************
  * MAC addressを指定. MAC1とMAC2で前後を分離. 16進数「0x」付け忘れ注意    *
  * {src, dst, x, y}で借り置きした.  例）srcのMAC address -> MAC1[0]MAC2[0]*
-**************************************************************************/
+ **************************************************************************/
 #define NTerminals  4               // 指定できるMAC address数
 uint16_t MAC1[NTerminals] = {0x0060, 0x0060, 0x0200, 0x0200};
 uint32_t MAC2[NTerminals] = {0xdd440bcb, 0xdd440c2f, 0x00000003, 0x00000004};
@@ -127,7 +125,8 @@ uint32_t MAC2[NTerminals] = {0xdd440bcb, 0xdd440c2f, 0x00000003, 0x00000004};
 /*******************************************
  * Open a socket for the network interface *
  *******************************************/
-int32_t open_socket(int32_t index, int32_t *rifindex) {
+int32_t open_socket(int32_t index, int32_t *rifindex)
+{
     unsigned char buf[MAX_PACKET_SIZE];
     int32_t i;
     int32_t ifindex;
@@ -173,10 +172,8 @@ int32_t open_socket(int32_t index, int32_t *rifindex) {
         _exit(1);
     };
 
-    /* flush all received packets. 
-     * raw-socket receives packets from all interfaces
-     * when the socket is not bound to an interface
-     */
+    /* flush all received packets.  raw-socket receives packets from
+       all interfaces when the socket is not bound to an interface */
     do {
         fd_set fds;
         struct timeval t;
@@ -196,37 +193,24 @@ int32_t open_socket(int32_t index, int32_t *rifindex) {
 }
 
 
-/**
- * Create an IPEC packet
- */
 
-//ssize_t createPacket(EtherPacket *packet, uint16_t destMAC1, uint32_t destMAC2,
-//        uint16_t srcMAC1, uint32_t srcMAC2, uint16_t type, uint32_t vlanTag,
-//       int32_t payload) {
+/*****************
+ * Create packet *
+ *****************/
 ssize_t createPacket(EtherPacket *packet, uint16_t destMAC1, uint32_t destMAC2,
-        uint16_t srcMAC1, uint32_t srcMAC2, uint32_t vlanTag, uint16_t type,
-        int32_t payload)
+        uint16_t srcMAC1, uint32_t srcMAC2, uint32_t vlanTag, uint16_t type, int32_t payload)
 {
-
-
-
     ssize_t packetSize = sizeof(EtherPacket);
-    //ssize_t packetSize = sizeof(EtherPacket)-20;
-    // ssize_t packetSize = payloadLength + sizeof(EtherPacket);
 
     memset(packet,0,sizeof(EtherPacket));
 
-    /* [memo]
-       uint8_t     不要
-       uint16_t    htons();
-       uint32_t    htonl();
-     */
+    /* [memo]   uint8_t     不要        *
+     *          uint16_t    htons();    *
+     *          uint32_t    htonl();    */
     packet->destMAC1 = htons(destMAC1);
     packet->destMAC2 = htonl(destMAC2);
     packet->srcMAC1 = htons(srcMAC1);
     packet->srcMAC2 = htonl(srcMAC2);
-
-    //memset(&payload,0,sizeof(payload));     //追記
 
 #ifdef VLAN
     packet->VLANTag = htonl(vlanTag);
@@ -246,7 +230,6 @@ ssize_t createPacket(EtherPacket *packet, uint16_t destMAC1, uint32_t destMAC2,
     packet->srcIP     = htonl(0x0a3a3c45);
     packet->dstIP     = htonl(0x0a3a3c48);
 #endif
-
 
 #ifdef UDP      //#ifdef IPv4 の packet->protocol を11に書き換えること
     packet->srcPort     = htons(0x0000);      //source port
@@ -268,77 +251,70 @@ ssize_t createPacket(EtherPacket *packet, uint16_t destMAC1, uint32_t destMAC2,
 #endif
 
     sprintf(packet->payload, "hello world");
-    //packet->payload = htonl(payload);
-    // strncpy(packet->payload, payload, packetSize);
-
     return packetSize;
 }
-
 int32_t lastPayload = -1;
 
 
-/**
- * Print IPEC packet content
- */
-void printPacket(EtherPacket *packet, ssize_t packetSize, char *message) {
-#ifdef VLAN
+
+/************************
+ * Print packet content *
+ ************************/
+void printPacket(EtherPacket *packet, ssize_t packetSize, char *message)
+{
+#ifdef VLAN     // show vlan ID
     printf("%s #%s (VLAN %d) from %04x%04x to %04x%04x\n",
-            message, packet->payload, ntohl(packet->VLANTag) & 0xFFF,
-        ntohs(packet->srcMAC1), ntohl(packet->srcMAC2),
-        ntohs(packet->destMAC1), ntohl(packet->destMAC2));
+            message, packet->payload, ntohl(packet->VLANTag) & 0xFFF
+            ,ntohs(packet->srcMAC1), ntohl(packet->srcMAC2),
+            ntohs(packet->destMAC1), ntohl(packet->destMAC2));
 #else
     printf("%s #%s from %04x%04x to %04x%04x\n",
-                message, packet->payload,
-        ntohs(packet->srcMAC1), ntohl(packet->srcMAC2),
-        ntohs(packet->destMAC1), ntohl(packet->destMAC2));
+            message, packet->payload,
+            ntohs(packet->srcMAC1), ntohl(packet->srcMAC2),
+            ntohs(packet->destMAC1), ntohl(packet->destMAC2));
 #endif
-    //lastPayload = ntohl(packet->payload);
- }
+}
 
 
- /*****************************
-  * Send packets to terminals *
-  *****************************/
- //void sendPackets(int32_t fd, int32_t ifindex, uint16_t SrcMAC1, uint32_t SrcMAC2,
- //    uint16_t DestMAC1, uint32_t DestMAC2, uint16_t type, uint32_t vlanTag,
- //    int32_t *count) {
- void sendPackets(int32_t fd, int32_t ifindex, uint16_t SrcMAC1, uint32_t SrcMAC2,
-     uint16_t DestMAC1, uint32_t DestMAC2, uint32_t vlanTag, uint16_t type,
-     int32_t *count)
+
+/*****************************
+ * Send packets to terminals *
+ *****************************/
+void sendPackets(int32_t fd, int32_t ifindex, uint16_t SrcMAC1, uint32_t SrcMAC2,
+        uint16_t DestMAC1, uint32_t DestMAC2, uint32_t vlanTag, uint16_t type,
+        int32_t *count)
 {
+    int32_t i;
+    unsigned char packet[MAX_PACKET_SIZE];
+
+    struct sockaddr_ll sll;
+    memset(&sll, 0, sizeof(sll));
+    sll.sll_family = AF_PACKET;
+    sll.sll_protocol = htons(ETH_P_ALL);   // Ethernet type = Trans. Ether Bridging
+    sll.sll_ifindex = ifindex;
+
+    ssize_t packetSize = createPacket((EtherPacket*)packet, DestMAC1, DestMAC2,
+            SrcMAC1, SrcMAC2, vlanTag, type,  (*count)++);
+
+    ssize_t sizeout = sendto(fd, packet, packetSize, 0,
+            (struct sockaddr *)&sll, sizeof(sll));
+
+    printPacket((EtherPacket*)packet, packetSize, "Sent:    ");
+    if (sizeout < 0) {
+        perror("sendto");
+    } else {
+        if (DEBUG) {
+            printf("%d bytes sent through interface (ifindex) %d\n",
+                    (int32_t)sizeout, (int32_t)ifindex);
+        }
+    }
+}
 
 
-     int32_t i;
-     unsigned char packet[MAX_PACKET_SIZE];
-     // unsigned char *payload = "Hello!";
 
-     struct sockaddr_ll sll;
-     memset(&sll, 0, sizeof(sll));
-     sll.sll_family = AF_PACKET;
-     sll.sll_protocol = htons(ETH_P_ALL);   // Ethernet type = Trans. Ether Bridging
-     sll.sll_ifindex = ifindex;
-
-     //ssize_t packetSize = createPacket((EtherPacket*)packet, DestMAC1, DestMAC2,
-     //      SrcMAC1, SrcMAC2, type, vlanTag, (*count)++);
-     ssize_t packetSize = createPacket((EtherPacket*)packet, DestMAC1, DestMAC2,
-             SrcMAC1, SrcMAC2, vlanTag, type,  (*count)++);
-
-     ssize_t sizeout = sendto(fd, packet, packetSize, 0,
-             (struct sockaddr *)&sll, sizeof(sll));
-
-     printPacket((EtherPacket*)packet, packetSize, "Sent:    ");
-     if (sizeout < 0) {
-         perror("sendto");
-     } else {
-         if (DEBUG) {
-             printf("%d bytes sent through interface (ifindex) %d\n",
-                     (int32_t)sizeout, (int32_t)ifindex);
-         }
-     }
- }
-
-//void sendTerms(int32_t fd, int32_t ifindex, uint16_t SrcMAC1, uint32_t SrcMAC2,
-//        uint16_t DestMAC1, uint32_t DestMAC2, uint16_t type, uint16_t vlanID) {
+/*********************************
+ * The terms of sending  packets *
+ *********************************/
 void sendTerms(int32_t fd, int32_t ifindex, uint16_t SrcMAC1, uint32_t SrcMAC2,
         uint16_t DestMAC1, uint32_t DestMAC2, uint16_t vlanID, uint16_t type) 
 {
@@ -352,7 +328,7 @@ void sendTerms(int32_t fd, int32_t ifindex, uint16_t SrcMAC1, uint32_t SrcMAC2,
 
     // Sending packets:
     for (; sendCount < MaxCommCount ;) {
-    //for (; sendCount < MaxCommCount && receiveCount < MaxCommCount;) {
+        //for (; sendCount < MaxCommCount && receiveCount < MaxCommCount;) {
         if (DestMAC2 != 0 && replyDelay <= 0) {
             int32_t currTime = time(NULL);
             if (currTime - lastTime >= Period) {
@@ -364,96 +340,42 @@ void sendTerms(int32_t fd, int32_t ifindex, uint16_t SrcMAC1, uint32_t SrcMAC2,
                 lastTime = currTime;
             }
         }
-    // Receiving packets:
-      /*  
-        ssize_t sizein = recv(fd, buf, MAX_PACKET_SIZE, 0);
-        if (sizein >= 0) {
-            EtherPacket *packet = (EtherPacket*) buf;
-            if (DestMAC2 == 0) {
-                DestMAC1 = ntohs(packet->srcMAC1);
-                DestMAC2 = ntohl(packet->srcMAC2);
-                replyDelay = InitialReplyDelay;
-            } else if (replyDelay > 0) {
-                replyDelay--;
-            }
-            printPacket(packet, sizein, "Received:");
-            receiveCount++;
-        } else {
-            usleep(10000); // sleep for 10 ms
-        }
-    */
     }
-}
+} // vimのインデント修正でずれる．なぜ？
 
 
-/****************
- * Main program *
- ****************/
-int32_t main(int32_t argc, char **argv)     // **argv = *argv[]
-{
-    int32_t ifindex;            //物理ifや論理ifに関連付けられる一意の識別番号 
-    int32_t myTermNum = 0;      //MAC1{}の何要素目か
-    int32_t destTermNum = 1;    //MAC2{}の何要素目か
-    int32_t ifnum = 5;          // 物理port番号??　  IFNAMEと何が違う??
-    uint16_t vlanID = 173;      //vlanIDを指定
-    int32_t i;                  // ???どこで使ってる？？
 
+    /****************
+     * Main program *
+     ****************/
+    int32_t main(int32_t argc, char **argv)     // **argv = *argv[]
+    {
+        int32_t ifindex;            //物理ifや論理ifに関連付けられる一意の識別番号 
+        int32_t myTermNum = 0;      //MAC1{}の何要素目か
+        int32_t destTermNum = 1;    //MAC2{}の何要素目か
+        int32_t ifnum = 5;          // 物理port番号??　  IFNAMEと何が違う??
+        uint16_t vlanID = 173;      //vlanIDを指定
+        //int32_t i;                  // ???どこで使ってる？？
 
-    // Get terminal and interface numbers（<-とは？） from the command line
-    // 実行時に引数で渡す
-    // int32_t count = 0; (original) createPacketと変数名重複するから変更した
-/*    int32_t counter = 0;
-    if (++counter < argc) {
-        myTermNum = atoi(argv[counter]);      // My terminal number
+        // Set locators and IDs using terminal number:
+        uint16_t SrcMAC1  = MAC1[myTermNum];
+        uint32_t SrcMAC2  = MAC2[myTermNum];
+        uint16_t DestMAC1 = MAC1[destTermNum];
+        uint32_t DestMAC2 = MAC2[destTermNum];
+
+        // 注）MAC addr->printfでのキャスト （uintからint）↲
+        // 変換指定子 %x ->小文字16進数表示．04-> 0フラグ↲
+        printf("p%dp1 terminal#=%d VLAN:%d srcMAC:%04x%04x destMAC:%04x%04x\n",
+                ifnum, myTermNum, vlanID,
+                (int32_t)SrcMAC1, (int32_t)SrcMAC2, (int32_t)DestMAC1, (int32_t)DestMAC2);
+
+        //socket生成
+        int32_t fd = open_socket(ifnum, &ifindex);
+
+        // Set non-blocking mode: receiveとsendができるように
+        //int32_t flags = fcntl(fd, F_GETFL, 0);
+        //fcntl(fd, F_SETFL, O_NONBLOCK | flags);
+
+        // ifindex ??
+        sendTerms(fd, ifindex, SrcMAC1, SrcMAC2, DestMAC1, DestMAC2, vlanID, ETH_P_Exp);
     }
-    if (myTermNum >= NTerminals || myTermNum < 0) {
-        printf("My terminal number (%d) too large\n", myTermNum);
-        myTermNum = 0;
-    }
-
-    if (++counter < argc) {
-        destTermNum = atoi(argv[counter]);    // Destination terminal number
-    }
-    if (destTermNum >= NTerminals || destTermNum < 0) {
-        printf("Destination terminal number (%d) too large\n", destTermNum);
-        destTermNum = 1;
-    }
-
-    if (++counter < argc) {
-        ifnum = atoi(argv[counter]);          // Interface number
-    }
-
-    if (++counter < argc) {
-        vlanID = atoi(argv[counter]);         // VLAN ID
-    }
-    if (vlanID < 1 || 4095 < vlanID) {
-        //printf("VLAN ID out of range (1..4095)\n", vlanID);       //before
-        printf("%"PRIu16"\n", vlanID);     // 書き足した．header fileもincludeに足した
-        vlanID = 1;
-    }
-*/
-
-    // Set locators and IDs using terminal number:
-    uint16_t SrcMAC1  = MAC1[myTermNum];
-    uint32_t SrcMAC2  = MAC2[myTermNum];
-    uint16_t DestMAC1 = MAC1[destTermNum];
-    uint32_t DestMAC2 = MAC2[destTermNum];
-
-    // MAC addr->printfする時にキャストしている  uintからint↲
-    // 変換指定子 %x ->小文字16進数表示．04-> 0フラグ↲
-    printf("p%dp1 terminal#=%d VLAN:%d srcMAC:%04x%04x destMAC:%04x%04x\n",
-            ifnum, myTermNum, vlanID,
-            (int32_t)SrcMAC1, (int32_t)SrcMAC2, (int32_t)DestMAC1, (int32_t)DestMAC2);
-
-    //socket生成
-    int32_t fd = open_socket(ifnum, &ifindex);
-
-    // Set non-blocking mode: receiveとsendができるように
-    //int32_t flags = fcntl(fd, F_GETFL, 0);
-    //fcntl(fd, F_SETFL, O_NONBLOCK | flags);
-
-    // ifindex ??
-    //sendReceive(fd, ifindex, SrcMAC1, SrcMAC2, DestMAC1, DestMAC2, ETH_P_Exp, vlanID);
-    //sendTerms(fd, ifindex, SrcMAC1, SrcMAC2, DestMAC1, DestMAC2, ETH_P_Exp, vlanID);
-    sendTerms(fd, ifindex, SrcMAC1, SrcMAC2, DestMAC1, DestMAC2, vlanID, ETH_P_Exp);
-}
